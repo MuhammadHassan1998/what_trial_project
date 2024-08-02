845ae0f82e62cb5ade492698f4c5117463df55c9
MuhammadHassan1998 <muhammadhhassan1998@gmail.com>
2024-08-02 18:13:39 +0500

    added Product model and serializers

--- what_api/products/models.py
+++ what_api/products/models.py
@@ -3,3 +3,13 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     pass
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    selected = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
